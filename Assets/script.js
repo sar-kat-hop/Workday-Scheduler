dc5841997d5395305a99006e8289feacb4e4cdb1
Serenity <sarah.k.hopper@gmail.com>
2022-11-19 22:54:33 -0800

    update code

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,34 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
+
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage.
+  
+// pseudo-code
+      // display list of standard business hours (9-5, I guess)
+      
+      // write fxn to check current time: If else? If #hour == current time, toggle .present class, else if #hour < current time, toggle .past class, else toggle .future class.
+
+
+
+
+      // on click of save button, store input in textarea corresponding to save button clicked locally. 
+
+      //
+
+      // declare var for user text input
+  var textInput = $("textarea");
+  
+  function saveInput() {
+
+  }
+
+
+
+
+
+
+  // HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
 
 
 
@@ -16,63 +37,33 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
+
+
+
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+
+
+  // TODO: Add code to display the current date in the header of the page. [DONE]
 
   // .append(content [, content ])
   // append( function )
 
-  $("#currentDay").append("<p>Today is </p>", dateToday);
+  $("#currentDay").append("<p>Today is</p>", dateToday);
       console.log(dateToday);
       // console.log("The time is " + currentTime);
   
 });
 
-//dayjs code
-
-//this isn't working, unclear why. 
-      // var localizedFormat = require('dayjs/plugin/localizedFormat')
-      // dayjs.extend(localizedFormat);
-      // var weekday = require('dayjs/plugin/weekday');
-      // dayjs.extend(weekday);
-
-// dayjs().format("LLLL");
+//dayjs code; localizedFormat plugin required, linked in index.html. 
 
 var dateToday = dayjs().format("LLLL");
 //user's current time, default is in 24-hr format, GMT. 
+
 // var currentTime = dayjs().hour();
 // var weekdayNum = dayjs().day();
 // var dayOfWeek = dayjs().toString();
-
-//LocalizedFormat plugin needed for this code to work...
-
-// function dayName () {
-//   if (weekdayNum == 0) {
-//     console.log("Sunday");
-//   }
-//   else if (weekdayNum == 1) {
-//     console.log("Monday");
-//   }
-//   else if (weekdayNum == 2) {
-//     console.log("Tuesday");
-//   }
-//   else if (weekdayNum == 3) {
-//     console.log("Wednesday");
-//   }
-//   else if (weekdayNum == 4) {
-//     console.log("Thursday");
-//   }
-//   else if (weekdayNum == 5) {
-//     console.log("Friday");
-//   }
-//   else {
-//     console.log("Saturday");
-//   };
-// };
-
-// });
-// toggleClass to toggle past, present, future css style to time blocks appropriately 
