f2f2f51d215de651f70e2217fa6b3592734cb433
Serenity <sarah.k.hopper@gmail.com>
2022-11-20 01:07:18 -0800

    update code

--- Assets/script.js
+++ Assets/script.js
@@ -1,31 +1,60 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
+// console.log(dayjs().hour());
+
+$(document).ready(function(){
+  var dateToday = dayjs().format("LLLL");
+  $("#currentDay").append("<p>Today is</p", dateToday);
+    console.log(dateToday);
+
+  // function clickSave() {
+
+  // };
+
+  // function saveInput() {
+
+  // };  
+
+});
+
+$(function() {
+  var currentHour = dayjs().hour().toString();
+  var scheduleHour = $(".hourText");
+
+  //need to make it possible to compare equality of currentHour and scheduleHour. Extract numbers from scheduleHour (remove am/pm) or convert currentHour format?
+  // var hourValue = scheduleHour.match(/(\d+)/);
+
+  console.log(currentHour);
+  console.log(hourValue); //not logging anything
+  // console.log(scheduleHour[0]);
+  $(scheduleHour).css("color", "blue");
 
 
-$(function () {
+
+
+  //attempts to grab ids beginning with "hour-" that I couldn't get to work:
+      // $("[id^=hour-").css("color", "blue");
+      // $("[id=hour-9").css("color", "blue");
+      // $("id[name^='hour']").css("color", "skyblue");
+      // $(".row time-block").css("font-size", "30 px");
+});
+
+
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage.
   
 // pseudo-code
-      // display list of standard business hours (9-5, I guess)
       
       // write fxn to check current time: If else? If #hour == current time, toggle .present class, else if #hour < current time, toggle .past class, else toggle .future class.
-
-
-
 
       // on click of save button, store input in textarea corresponding to save button clicked locally. 
 
       //
 
       // declare var for user text input
-  var textInput = $("textarea");
+  // var textInput = $("textarea");
   
-  function saveInput() {
+  // function saveInput() {
 
-  }
-
-
-
-
+// )};
 
 
   // HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
@@ -38,32 +67,13 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   
+// pseudo-code
+// If else? If #hour == current time, toggle .present class, else if #hour < current time, toggle .past class, else toggle .future class.
+
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
 
 
 
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-
-
-  // TODO: Add code to display the current date in the header of the page. [DONE]
-
-  // .append(content [, content ])
-  // append( function )
-
-  $("#currentDay").append("<p>Today is</p>", dateToday);
-      console.log(dateToday);
-      // console.log("The time is " + currentTime);
-  
-});
-
-//dayjs code; localizedFormat plugin required, linked in index.html. 
-
-var dateToday = dayjs().format("LLLL");
-//user's current time, default is in 24-hr format, GMT. 
-
-// var currentTime = dayjs().hour();
-// var weekdayNum = dayjs().day();
-// var dayOfWeek = dayjs().toString();
