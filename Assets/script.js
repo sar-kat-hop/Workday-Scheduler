e0cc75d452aeae2040adb2135fc05576dc897a85
Serenity <sarah.k.hopper@gmail.com>
2022-11-20 21:56:28 -0800

    update code

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,34 @@
 console.log("Current hour:", dayjs().hour()); 
-//default is 24-hr format but can be converted to local format, which I think needs to happen for comparing equality to time-block hours (unless those get changed to 24-hr or get assigned hidden 24-hr values)
+
+// var am9 = $("#input-1");
+// var am10 = $("#input-2");
+// var am11 = $("#input-3");
+// var pm12 = $("#input-4");
+// var pm1 = $("#input-5");
+// var pm2 = $("#input-6");
+// var pm3 = $("#input-7");
+// var pm4 = $("#input-8");
+// var pm5 = $("#input-9");
+// var event9 = $(am9).data();
+// var event10 = $(am10).data();
+// var event11 = $(am11).data();
+// var event12 = $(pm12).data();
+// var event13 = $(pm1).data();
+// var event14 = $(pm2).data();
+// var event15 = $(pm3).data();
+// var event16 = $(pm4).data();
+// var event17 = $(pm5).data();
 
 var timeBlockDiv = $("div[id^=hour-]"); 
 var saveBtnEl = $("button[class^=btn]");
+var inputEl = $("[id^=input-]"); 
+var userInput = $("[type='text']").data();
+var savedInput = localStorage.getItem(userInput);
+var currentHour = dayjs().hour();
 
-// declaring these globally doesn't appear to be enough for fxns below to be able to access
-// var userInput = $("textarea[id^=input-]").val();
-// var inputEl = $("[id^=input-]"); 
-
+// var schedEvents = {
+  
+// };
 // console.log(userInput);
 
 // console.log("div[id^=hour-1]", timeBlockDiv); 
@@ -15,62 +36,50 @@ var saveBtnEl = $("button[class^=btn]");
 // var saveInput = localStorage.setItem(inputEl, userInput);
 // var displayInput = localStorage.getItem(saveInput);
 // var inputEl = $("textarea[class^=input]");
-    // console.log("textarea[class^=input]", inputEl);
+// console.log("textarea[class^=input]", inputEl);
 
 $(document).ready(function() {
+
   var dateToday = dayjs().format("LLLL");
+
   $("#currentDay").append("<p>Today is</p", dateToday);
   console.log("Today's date is", dateToday); 
-  // displayLastSaved();
+
+  });
+
+$(function renderSaved() {
+  $(userInput).each(function(i) {
+    // for (variable of iterable) {
+
+    // }
+  })
 });
 
-//this WAS saving input but isn't anymore; when working, it needed to only save input for each block individually
+$(function highlight() {
+  $(timeBlockDiv).each(function(i) {
+    if (this.value < currentHour) {
+      $(this).toggleClass(".past");
+    } else if (this.value > currentHour) {
+      $(this).toggleClass(".future");
+    } else {
+      $(this).toggleClass("present");
+    }
+  });
+});
+
 $(function saveInput() {
     saveBtnEl.on("click", function(event) {
       event.preventDefault();
-      var userInput = $("textarea[id^=input-]").val();
-      var inputEl = $("[id^=input-]"); 
       
       // SYNTAX:
       // localStorage.setItem(key, value);
       // let lastname = localStorage.getItem(key);
-      localStorage.setItem(inputEl, userInput);
-      console.log("input:", userInput);
+      localStorage.setItem(inputEl, savedInput);
 
-      displayLastSaved();
     });
   });
 
 // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
-$(function displayLastSaved() {
-  var displayInput = localStorage.getItem("userInput");
-  var inputEl = $("[id^=input-]"); 
-
-  $(inputEl).append(displayInput);
-    // for (let i = 0; i < inputEl.length; i++) {
-    //   $(inputEl).children().append(displayInput);
-          console.log(displayInput);
-    // };
-  });
-
-// $(function saveInput() {
-//   saveBtnEl.on("click", function() {
-//     // if (userInput != "") {
-//       // var userInput = $("textarea]").val();
-//       localStorage.setItem("userInput", userInput);
-//         console.log("input:", userInput);
-//       // };
-//     });
-//   });
 
 // pseudo-code
       // write fxn to check current time: If else? If #hour == current time, toggle .present class, else if #hour < current time, toggle .past class, else toggle .future class.
-
-// $(function() {
-//   var currentHour = dayjs().hour() //.toString();
-  // console.log(currentHour);
-  // var scheduleHour = $(".hourText").toNum();
-  // var scheduleHour = $(".hourText").text();
-  // var parsedHour = parseInt(scheduleHour);
-  // console.log(parsedHour); //logging NaN... can't be converted to number?
-  // var hourValue = scheduleHour.match(/(\d+)/);
