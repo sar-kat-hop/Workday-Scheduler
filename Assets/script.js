eb5c6aa4ea0d9fb8ef3cbf63db3a5c046c7065f1
Serenity <sarah.k.hopper@gmail.com>
2022-11-20 18:53:19 -0800

    update code (save input button semi-working)

--- Assets/script.js
+++ Assets/script.js
@@ -2,53 +2,56 @@ console.log("Current hour:", dayjs().hour());
 //default is 24-hr format but can be converted to local format, which I think needs to happen for comparing equality to time-block hours (unless those get changed to 24-hr or get assigned hidden 24-hr values)
 
 var timeBlockDiv = $("div[id^=hour-]"); 
-    console.log("div[id^=hour-1]", timeBlockDiv); 
-// var inputEl = $("div[id^=hour-].input"); 
-var inputEl = $("textarea[class^=input]");
-    console.log("textarea[class^=input]", inputEl);
-var input = inputEl.val();
-    console.log("input:", input);
-var saveBtnEl = $(".saveBtn");
-    console.log("saveBtnEl", saveBtnEl);
+var inputEl = $("textarea[id^=input-]"); 
+var saveBtnEl = $("button[class^=btn]");
+var userInput = $("texarea[id^=input-]").val();
+
+// console.log("div[id^=hour-1]", timeBlockDiv); 
+// console.log("saveBtnEl", saveBtnEl);
 // var saveInput = localStorage.setItem(inputEl, userInput);
 // var displayInput = localStorage.getItem(saveInput);
-    
-  $(document).ready(function() {
-    var dateToday = dayjs().format("LLLL");
-    $("#currentDay").append("<p>Today is</p", dateToday);
-          console.log("Today's date is", dateToday);
-      
-      (function displayLastSaved() {
-        var userInput = localStorage.getItem(input); 
-        // var displayInput = 
-        $(inputEl).append(userInput);
-          console.log(userInput);
-    }); 
+// var inputEl = $("textarea[class^=input]");
+    // console.log("textarea[class^=input]", inputEl);
+
+$(document).ready(function() {
+  var dateToday = dayjs().format("LLLL");
+  $("#currentDay").append("<p>Today is</p", dateToday);
+  console.log("Today's date is", dateToday);
+});
+
+$(function() {
+  saveBtnEl.on("click", function(event) {
+    event.preventDefault();
+  });
+});
+
+// TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage.
+// HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
+
+// SYNTAX:
+// localStorage.setItem(key, value);
+// let lastname = localStorage.getItem(key);
+
+//this is saving input but needs to only save input for each block individually
+$(function saveInput() {
+  saveBtnEl.on("click", function() {
+    // if (userInput != "") {
+      // var userInput = $("textarea]").val();
+      localStorage.setItem("userInput", userInput);
+        console.log("input:", userInput);
+      // };
+    });
   });
 
-    $(function() {
-      saveBtnEl.on("click", function(event) {
-        event.preventDefault();
-    });
-
-  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage.
-  // HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
-  
-        // SYNTAX:
-        // localStorage.setItem(key, value);
-        // let lastname = localStorage.getItem(key);
-
-    $(function saveInput() {
-      saveBtnEl.on("click", function() {
-        var userInput = input;
-        // var userInput = $(this.input).val(); 
-        //trying to point to save button's corresponding text area, all of which I've given the .input class to
-        localStorage.setItem(inputEl, userInput);
-          console.log(userInput);
-        // localStorage.setItem(savedInput.value);
-        });
-      });
-    });
+//this is displaying saved input, but it's displaying it on ALL available inputEls
+$(function displayLastSaved() {
+  var displayInput = localStorage.getItem("userInput");
+  $(inputEl).append(displayInput);
+    // for (let i = 0; i < inputEl.length; i++) {
+    //   $(inputEl).children().append(displayInput);
+          console.log(displayInput);
+    // };
+});
 
 // $(function() {
 //   var currentHour = dayjs().hour() //.toString();
@@ -69,7 +72,3 @@ var saveBtnEl = $(".saveBtn");
   // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
   
 // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
-
-
-
-
